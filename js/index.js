f1713721b212c4602f605324c4ae7b5d735e3358
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2020-07-24 19:51:43 +0900

    fixed js

--- js/index.js
+++ js/index.js
@@ -25,13 +25,35 @@ $(document).ready(function(){
         dots: true,
         slidesToShow: 1,
         autoplaySpeed: 5000,
-        speed: 500,
+        speed: 300,
         infinite: true,
         cssEase: 'linear',
         arrows: true,
         swipe: true,
         centerMode: true,
     });
+});
+// 紹介文がアクティブになっている画像を拡大する
+$(".content_members_slide").on('afterChange', function(slick, currentSlide){
+    // 先に変更をリセット
+    $('.members_img').css({'width': '', 'height': ''});
+
+    var slideNum = currentSlide.currentSlide;
+    var img = $('input[value="' + slideNum + '"].slide_num').prev('img');
+    var currentHeight = img.height();
+    var currentWidth = img.width();
+    if (currentHeight > 121) {
+        img.height(currentHeight + 50);
+        img.width(currentWidth + 50);
+    } else {
+        img.height(currentHeight + 20);
+        img.width(currentWidth + 20);
+    }
+});
+// メンバー紹介 画像クリックで紹介文をスライドさせる
+$('.members_img').click(function(){
+    var slideNum = $(this).next('input').val()
+    $(".content_members_slide").slick('slickGoTo', slideNum);
 });
 
 // ヘッダの動き
